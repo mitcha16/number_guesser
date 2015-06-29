a5bfc97df366c58ae36b1aa74f8757a527bbaa97
mitchashby16 <mitchashby16@gmail.com>
2015-06-29 12:31:34 -0600

    generates random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
+def random_number_generator
+  rand(1..100)
+end
+num = random_number_generator
 get '/' do
-  "Hello, World!"
+  "The SECRET NUMBER is #{num}"
 end
